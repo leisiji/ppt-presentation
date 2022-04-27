24f97545973abd342dc298454638188f6278821d
leisiji <xuelinye9@gmail.com>
2022-04-27 19:24:10 +0800

    update memory-barrier ppt

---
+++ ARMv8-memory-barrier/test/atomic-release-cosume.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdatomic.h>
+#include <assert.h>
+#include <pthread.h>
+
+#define STRING "hello world"
+int data = 0;
+atomic_uintptr_t ptr;
+
+static void *producer(void *d)
+{
+	const char *p = strdup(STRING);
+	data = 42;
+	atomic_store_explicit(&ptr, (long unsigned int)p, memory_order_release);
+	return NULL;
+}
+
+static void *consumer(void *d)
+{
+	const char *p2 = NULL;
+	while (!(p2 = (const char *)atomic_load_explicit(&ptr, memory_order_consume))) { }
+	assert(!strcmp(p2, STRING)); // never fail
+	assert(data == 42); // may fail
+	return NULL;
+}
+
+int main (int argc, char *argv[])
+{
+	pthread_t t1;
+	pthread_t t2;
+	pthread_create(&t1, NULL, producer, NULL);
+	pthread_create(&t2, NULL, consumer, NULL);
+	return 0;
+}
